b37eafaaa534416d0ff68e53f1d73134c999c805
conny <conny@musma.net>
2024-10-16 09:23:43 +0900

    design account form

--- src/components/forms/account-form.tsx
+++ src/components/forms/account-form.tsx
@@ -3,8 +3,28 @@ import {useEffect, useState} from 'react';
 import Avatar from '@/app/(main)/(pages)/account/_components/avatar';
 import {useProfileContext} from '@/providers/profile-provider';
 import {useAuthContext} from '@/providers/auth-provider';
+import {Button} from '@/components/ui/button';
+import {z} from 'zod';
+import {useForm} from 'react-hook-form';
+import {zodResolver} from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import {login} from '@/app/login/_actions/login-action';
+import {ToastAction} from '@/components/ui/toast';
+import * as React from 'react';
+import {Input} from '@/components/ui/input';
 
-// ...
+const formSchema = z.object({
+  email: z.string(),
+  fullName: z.string(),
+  username: z.string(),
+});
 
 export default function AccountForm() {
   const authContext = useAuthContext();
@@ -13,6 +33,23 @@ export default function AccountForm() {
   const [username, setUsername] = useState<string | null>(null);
   const [website, setWebsite] = useState<string | null>(null);
   const [avatar_url, setAvatarUrl] = useState<string | null>(null);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: authContext.user?.email ?? '',
+      fullName: profileContext.fullname ?? '',
+      username: profileContext.username ?? '',
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    await profileContext.updateProfile({
+      fullname: values.fullName,
+      username: values.username,
+      website: website,
+      avatar_url: avatar_url,
+    });
+  }
 
   useEffect(() => {
     setAvatarUrl(profileContext.avatar_url);
@@ -22,7 +59,7 @@ export default function AccountForm() {
   }, [profileContext]);
 
   return (
-    <div className="form-widget">
+    <div className="flex flex-col items-center justify-center mt-10">
       <Avatar
         uid={authContext.user?.id ?? null}
         url={avatar_url}
@@ -37,46 +74,52 @@ export default function AccountForm() {
           });
         }}
       />
+      <Form {...form}>
+        <form className={'w-2/3'} onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name={'email'}
+            render={({field}) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input {...field} value={authContext.user?.email} disabled />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name={'fullName'}
+            render={({field}) => (
+              <FormItem>
+                <FormLabel>fullName</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name={'username'}
+            render={({field}) => (
+              <FormItem>
+                <FormLabel>username</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
 
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          id="email"
-          type="text"
-          value={authContext.user?.email}
-          disabled
-        />
-      </div>
-      <div>
-        <label htmlFor="fullName">Full Name</label>
-        <input
-          id="fullName"
-          type="text"
-          value={fullname || ''}
-          onChange={e => setFullname(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          value={username || ''}
-          onChange={e => setUsername(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="website">Website</label>
-        <input
-          id="website"
-          type="url"
-          value={website || ''}
-          onChange={e => setWebsite(e.target.value)}
-        />
-      </div>
-
-      <div>
-        <button
+      <div className={'flex flex-row w-full justify-evenly mt-10'}>
+        <Button
           className="button primary block"
           onClick={() =>
             profileContext.updateProfile({
@@ -89,14 +132,12 @@ export default function AccountForm() {
           disabled={profileContext.loading}
         >
           {profileContext.loading ? 'Loading ...' : 'Update'}
-        </button>
-      </div>
+        </Button>
 
-      <div>
         <form action="/api/auth/signout" method="post">
-          <button className="button block" type="submit">
+          <Button className="button block" type="submit">
             Sign out
-          </button>
+          </Button>
         </form>
       </div>
     </div>
